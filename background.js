a4139458ad8ebd547fbdf99895b86665893bb158
Prajjwal Yadav <prajjyadav0904@gmail.com>
2020-10-02 03:51:57 +0530

    added uptime functionality

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 let date = Date.now();
+var date2 = Date.now();
 let countdownMaxInMin = 20;
 let countdownMaxInSec = countdownMaxInMin * 60;
 let countdownMaxInMS = countdownMaxInSec * 1000;
@@ -13,14 +14,17 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 chrome.storage.local.set({
   date: date,
   isPaused: false,
+  startTime: false,
   countdownMaxInMin: countdownMaxInMin
 });
+
 
 clearAndCreateAlarm(countdownMaxInMin,countdownMaxInMin);
 
 // Add a listener for when the alarm is up.
 // When the alarm is up, create a window with timer.html.
 chrome.alarms.onAlarm.addListener(function(alarm) {
+    
   if (alarm.name == 'eyeRestAlarm' + date) {
     let nextAlarmTime = alarm.scheduledTime + countdownMaxInMS;
     chrome.storage.local.set({nextAlarmTime: nextAlarmTime});
@@ -44,3 +48,7 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
     });
   }
 });
+
+chrome.runtime.onStartup.addListener(function() {
+ chrome.storage.local.set({upTime: Date.now()});
+})
